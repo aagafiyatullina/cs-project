b25256f81ba50f4343c9ebe2c30291bfdd8b3fe6
aagafiyatullina <34176255+aagafiyatullina@users.noreply.github.com>
2018-06-02 09:41:46 +0300

    Add files via upload

---
+++ object_list.js
@@ -0,0 +1,103 @@
+ymaps.ready(init);
+
+function init() {
+
+    var myMap = new ymaps.Map('map', {
+            center: [50.443705, 30.530946],
+            zoom: 14
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        menu = $('<ul class="menu"></ul>');
+        
+    for (var i = 0, l = groups.length; i < l; i++) {
+        createMenuGroup(groups[i]);
+    }
+
+    myMap.events.add('click', function (e) {
+        var coords = e.get('coords');
+
+        if (myPlacemark) {
+            myPlacemark.geometry.setCoordinates(coords);
+        }
+        else {
+            myPlacemark = createPlacemark(coords);
+            myMap.geoObjects.add(myPlacemark);
+            myPlacemark.events.add('dragend', function () {
+                getAddress(myPlacemark.geometry.getCoordinates());
+            });
+        }
+        getAddress(coords);
+    });
+
+    function createPlacemark(coords) {
+        return new ymaps.Placemark(coords, {
+            iconCaption: 'поиск...'
+        }, {
+            preset: 'islands#violetDotIconWithCaption',
+            draggable: true
+        });
+    }
+
+    function getAddress(coords) {
+        myPlacemark.properties.set('iconCaption', 'поиск...');
+        ymaps.geocode(coords).then(function (res) {
+            var firstGeoObject = res.geoObjects.get(0);
+
+            myPlacemark.properties
+                .set({
+                    iconCaption: [
+                        firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
+                        firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
+                    ].filter(Boolean).join(', '),
+                    balloonContent: firstGeoObject.getAddressLine()
+                });
+        });
+    }
+
+    function createMenuGroup (group) {
+        var menuItem = $('<li><a href="#">' + group.name + '</a></li>'),
+            collection = new ymaps.GeoObjectCollection(null, { preset: group.style }),
+            submenu = $('<ul class="submenu"></ul>');
+
+        myMap.geoObjects.add(collection);
+        menuItem
+            .append(submenu)
+            .appendTo(menu)
+            .find('a')
+            .bind('click', function () {
+                if (collection.getParent()) {
+                    myMap.geoObjects.remove(collection);
+                    submenu.hide();
+                } else {
+                    myMap.geoObjects.add(collection);
+                    submenu.show();
+                }
+            });
+        for (var j = 0, m = group.items.length; j < m; j++) {
+            createSubMenu(group.items[j], collection, submenu);
+        }
+    }
+
+    function createSubMenu (item, collection, submenu) {
+        var submenuItem = $('<li><a href="#">' + item.name + '</a></li>'),
+            placemark = new ymaps.Placemark(item.center, { balloonContent: item.name });
+
+        collection.add(placemark);
+        submenuItem
+            .appendTo(submenu)
+            .find('a')
+            .bind('click', function () {
+                if (!placemark.balloon.isOpen()) {
+                    placemark.balloon.open();
+                } else {
+                    placemark.balloon.close();
+                }
+                return false;
+            });
+    }
+
+    menu.appendTo($('body'));
+    myMap.setBounds(myMap.geoObjects.getBounds());
+    
+}
